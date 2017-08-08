444fb09e08b30e2d6c24f957ba56bddc291e4cd4
Johanna Staedel <bluecherryme@gmail.com>
2017-08-08 14:25:16 -0600

    saving and retrieving of data is possible

--- whiteboard/src/components/Whiteboard/Whiteboard.js
+++ whiteboard/src/components/Whiteboard/Whiteboard.js
@@ -24,28 +24,32 @@ class Whiteboard extends Component {
       this.save = this.save.bind(this);
       this.erase = this.erase.bind(this);
       this.displayThumb = this.displayThumb.bind(this);
+      this.retrieve = this.retrieve.bind(this);
   }
 
   // componentDidMount() {
   //   wsClient.on('addItem', item => this.setState({items: this.state.items.concat([item])}));
   // }
 
+  retrieve(data){
+      let canvas = document.getElementById('canvas');
+      let ctx = canvas.getContext("2d"); 
+      ctx.putImageData(data,0,0);
+  }
+
   displayThumb(){
-        // let canvas = document.getElementById('thumb');
-        // let ctx = canvas.getContext("2d"); 
-        //console.log(this.state.URL);
-        //ctx.drawImage(this.state.URL,0,0,0,0,150,150);
-        //ctx.putImageData(this.state.data,0,0,0,0,150,150)
-        document.getElementById("thumb").style.border = "1px solid";        
+        document.getElementById("thumb").style.border = "1px solid";  
+        //replace this.state.URL with data from database      
         document.getElementById("thumb").src = this.state.URL;
         document.getElementById("thumb").style.display = "inline";
-        
   }
 
   save() {
         let canvas = document.getElementById('canvas');
         let ctx = canvas.getContext("2d"); 
-        this.setState({URL:canvas.toDataURL(), data:ctx.getImageData(0,0,500,500)})
+        // action creator or axios call here instead of setting state
+        this.setState({URL:canvas.toDataURL(), data:ctx.getImageData(0,0,500,500)});
+        this.erase();
     }
 
     erase() {
@@ -127,7 +131,7 @@ render() {
             <button onClick={()=>this.save()}>Save</button>
             <button onClick={()=>this.erase()}>Erase</button>
         </div>
-        <img id='thumb' style={{display: 'none', height: '150px', width:'150px'}}></img>
+        <img onClick={()=>this.retrieve(this.state.data)} id='thumb' style={{display: 'none', height: '150px', width:'150px'}}></img>
       </div>
     );
   }
