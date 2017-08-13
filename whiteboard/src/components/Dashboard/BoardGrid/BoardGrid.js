2ee5f2275fefb3ab2be6576f5e3ccbeb4b544f03
Beth <mrsbethtelford@gmail.com>
2017-08-13 17:29:32 -0600

    board grid styled and filtering according to url params

--- whiteboard/src/components/Dashboard/BoardGrid/BoardGrid.js
+++ whiteboard/src/components/Dashboard/BoardGrid/BoardGrid.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { withRouter, Link } from 'react-router-dom';
+
+import './BoardGrid.css';
 
 class BoardGrid extends Component {
   render() {
     console.log('grid props', this.props);
     return (
       <div id='BoardGrid'>
-        {JSON.stringify(this.props.boards)}
+        {this.props.boards.map((board, i) => {
+          {/* console.log(this.props.boards) */}
+          let img = new Image();
+          img.src = board.canvas;
+          let gridPicStyle = {
+            backgroundImage: `url(${img.src})`
+          }
+          if (board.projectid == this.props.match.params.projectid) {
+            return <Link to={`/board/${board.id}`} key={i} className='grid_tile'>
+              <div style={gridPicStyle} className='grid_pic'></div>
+              <div className='grid_picTransparent'></div>
+              <p>{board.name}</p>
+            </Link>
+          }
+        })
+        }
       </div>
     )
   }
@@ -16,4 +34,4 @@ function mapStateToProps(state) {
     boards: state.userData.boards
   }
 }
-export default connect(mapStateToProps)(BoardGrid);
+export default withRouter(connect(mapStateToProps)(BoardGrid));
