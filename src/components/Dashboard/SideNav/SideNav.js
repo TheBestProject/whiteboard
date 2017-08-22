c7e827b0ef9252f72d8c2a09296f2ef0552298fc
Beth <mrsbethtelford@gmail.com>
2017-08-21 19:38:35 -0600

    fixed logout buttons

--- src/components/Dashboard/SideNav/SideNav.js
+++ src/components/Dashboard/SideNav/SideNav.js
@@ -96,7 +96,7 @@ class SideNav extends Component {
           <div className='sideNav_username'>
             <h2>{username}</h2>
             <div className='sideNav_logoutEdit'>
-              <a href='https://whiteboarders.herokuapp.com//auth0/logout'>logout</a>
+              <a href='https://whiteboarders.herokuapp.com/auth0/logout'>logout</a>
               <p onClick={() => this.setState({profileFlag: true})}>edit</p>
             </div>
           </div>
